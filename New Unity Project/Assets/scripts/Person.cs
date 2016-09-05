27fcbd718b265d2dd16a9ed9d98b7aa2d80340e8
libo123459 <libo123459@126.com>
2016-09-05 20:09:29 +0800

    new project

---
+++ New Unity Project/Assets/scripts/Person.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Person : MonoBehaviour {
+    public string ClanTag = null;
+    public List<Person> Grand = new List<Person>();
+    public List<Person> BroSis = new List<Person>();
+    public List<Person> Parent = new List<Person>();
+    public string lastname = null;
+    public string firstname = null;
+    public int sex = 0;
+    public bool single = true;
+    public int age = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ New Unity Project/Assets/scripts/social.cs
@@ -0,0 +1,271 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class social : MonoBehaviour {
+    public Person person;
+    private string[] clan = new string[] { "Smith", "Jones", "Williams", "Taylor", "Brown", "Wilson"};
+    private string[] MaleNames = new string[] { "Byron","Dick","Evan","James","Joseph","Alex", "Kim", "Bill", "Daniel", "David", "Paul", "Joe", "Peter", "Tom" };
+    private string[] FemaleNames = new string[] { "Mary", "Jenny", "Mila", "Gianna", "Elissa", "Tiffany", "Sarah", "Jessica", "Lisa", "Jennifer", "Eva", "Alice", "Anne", "Judy" };
+    private List<string> Clan = new List<string>();
+    private List<Person> Popu = new List<Person>();
+    
+    
+
+    private List<List<Person>> AllFam = new List<List<Person>>();
+    private int FamNum = 0;
+
+    // Use this for initialization
+    void Start () {
+       
+        intiTheClan();
+        intiThePopu();
+        makeFamily();
+        evolution();
+        printInfo();
+	}
+
+    void intiTheClan()                                                 ///初始化家族
+    {
+        for (int i = 0; i < clan.Length; i++)
+        {
+            Clan.Add(clan[i]);
+        }
+    }
+
+    void intiThePopu()                                                 ///初始化人口数量
+    {
+        for (int i = 0; i < Clan.Count; i++)  
+        {
+            for (int j = 0; j < 2; j++)                                ///给每个家族安排两个人
+            {
+                Person newPerson = new Person();
+                newPerson.single = true;                               ///是否单身
+                newPerson.sex = Mathf.Abs(1-j);                        ///1为男性，0为女性
+                giveTheName(newPerson,Clan[i]);
+                for (int k = 0; k < newPerson.ClanTag.Length; k++)   ///给每个人打上家族标签
+                {
+                    if (newPerson.ClanTag == null)
+                    {
+                        newPerson.ClanTag = Clan[i];
+                        break;
+                    }
+                    else {
+                        continue;
+                    }
+                }
+                Popu.Add(newPerson);                                                    ///整个人口的List
+            }
+        }
+        for (int i = 0; i < Popu.Count; i++)                                             ///初始人口的brolist
+        {
+            Person theperson = Popu[i];
+            for (int j = 0; j < Popu.Count; j++)
+            {
+                if (i != j)
+                {
+                    if (theperson.ClanTag == Popu[j].ClanTag)
+                    {
+                        theperson.BroSis.Add(Popu[j]);
+                    }
+                } else
+                {
+                    continue;
+                }
+            }
+        }
+        print(Popu.Count);
+    }
+
+    void makeFamily()                                                                   ///组建家庭
+    {
+        for (int i = 0; i < Popu.Count; i++)
+        {
+            Person thePerson = Popu[i];                                                 ///从头开始遍历
+            if (Popu[i].single == true)
+            {
+                for (int j = 0; j < Popu.Count; j++)
+                {
+                    int married = Random.Range(0, 100);                                     ///Marry的几率
+                    if (married <= 20)
+                    {
+                        Person other = Popu[j];                                             ///目标对象
+                        if (i != j)                                                         ///在不是自身的情况下
+                        {
+                            bool check = CheckIfCanMarry(thePerson,other);
+                            if (thePerson.sex != other.sex && other.single == true && check == true)       ///两人单身且异性
+                            {
+                                thePerson.single = false;
+                                other.single = false;
+                                List<Person> family = new List<Person>();
+                                family.Add(thePerson);
+                                family.Add(other);                                          ///两人组成一个家庭
+                                AllFam.Add(family);                                 ///第famnum号家庭
+                                FamNum++;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    bool CheckIfCanMarry(Person thePerson,Person other)
+    {
+        bool NoSameGra = false;///爷爷辈没有同一个人
+        bool NoGraUncle = false;///爷爷辈不是兄弟姐们
+        bool NoSamePar = false;///父母没有同一个人
+        bool NoUncle = false;///父母不是兄弟姐妹
+        bool NoParInGra = false; ///父母不是爷爷辈
+        bool NoGraInPar = false;///爷爷辈不是父母
+
+        for (int i = 0; i < thePerson.Grand.Count; i++)                                 
+        {
+            for (int j = 0; j < other.Grand.Count; j++)                                   ///爷爷辈没有同一个人
+            {
+                if (thePerson.Grand[i] != other.Grand[j])
+                {
+                    continue;
+                }
+                else {
+                    NoSameGra = true;
+                }
+            }
+
+            for (int j = 0; j < thePerson.Grand[i].BroSis.Count; j++)                    ///爷爷辈不是兄弟姐们
+            {
+                for (int k = 0; k < other.Grand.Count; k++)
+                {
+                    if (thePerson.Grand[i].BroSis[j] != other.Grand[k])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        NoGraUncle = true;
+                    }
+                }
+            }
+
+            for (int j = 0; j < other.Parent.Count; j++)                                       ///父母不是爷爷辈                     
+            {
+                if (thePerson.Grand[i] != other.Parent[j])
+                {
+                    continue;
+                }
+                else
+                {
+                    NoParInGra = true;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < thePerson.Parent.Count; i++)                               ///父母没有同一个人
+        {
+            for (int j = 0; j < other.Parent.Count; j++)
+            {
+                if (thePerson.Parent[i] != other.Parent[j])
+                {
+                    continue;
+                }
+                else
+                {
+                    NoSamePar = true;
+                }
+            }
+
+            for (int j = 0; j < thePerson.Parent[i].BroSis.Count; j++)                 ///父母不是兄弟姐妹
+            {
+                for (int k = 0; k < other.Parent.Count; k++)
+                {
+                    if (thePerson.Parent[i].BroSis[j] != other.Parent[k])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        NoUncle = true;
+                    }
+                }
+            }
+
+            for (int j = 0; j < other.Grand.Count; j++)                                 ///爷爷辈不是父母
+            {
+                if (thePerson.Parent[i] != other.Grand[j])
+                {
+                    continue;
+                }
+                else
+                {
+                    NoGraInPar = true;
+                }
+            }
+        }
+
+        if (NoSameGra == true || NoGraUncle == true || NoSamePar == true || NoUncle == true || NoParInGra == true || NoGraInPar == true)
+        {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    void giveTheName(Person thePerson,string lastname)                             ///生成姓名
+    {
+        if (thePerson.sex == 0)
+        {
+            thePerson.firstname = FemaleNames[Random.Range(0, FemaleNames.Length)];
+            thePerson.lastname = lastname;
+        }
+        else {
+            thePerson.firstname = MaleNames[Random.Range(0, MaleNames.Length)];
+            thePerson.lastname = lastname;
+        }
+    }
+
+    void evolution()                                         ///时间推演(生孩子)      
+    {
+        for (int i = 0; i < AllFam.Count; i++)
+        {
+            Person newPerson = new Person();
+            newPerson.sex = Random.Range(-1,1);
+            newPerson.single = true;
+            
+            for (int j = 0; j < AllFam[i].Count; j++)
+            {
+                newPerson.ClanTag = AllFam[i][j].ClanTag;
+                newPerson.Parent.Add(AllFam[i][j]);                               ///孩子的父母加入孩子的parent数组
+                for (int k = 0; k < AllFam[i][j].Grand.Count; k++)
+                {
+                    newPerson.Grand.Add(AllFam[i][j].Parent[k]);                   ///父母的父母加入孩子的Grand数组
+                }
+                
+                if (AllFam[i][j].sex == 1)
+                {
+                    giveTheName(newPerson, AllFam[i][j].lastname);
+                }
+            }
+            AllFam[i].Add(newPerson);
+        }
+    }
+
+
+    void printInfo()                                         ///输出信息
+    {
+        print("The families count is" + AllFam.Count);
+        
+        for (int i = 0; i < AllFam.Count; i++)
+        {
+            print("The Num" + i + " :" + AllFam[i][0].firstname + AllFam[i][0].lastname + " and " + AllFam[i][1].firstname + AllFam[i][1].lastname + " Kid : " + AllFam[i][2].firstname + AllFam[i][2].lastname);
+        }
+    }
+
+    
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
